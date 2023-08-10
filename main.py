24cade61fcd923c9258dbf49f04a4fa979ca69d4
Huazai <i@huazai.me>
2023-08-10 09:21:26 +0800

    优化

--- main.py
+++ main.py
@@ -1,76 +1,65 @@
 #!/usr/bin/env python3
 # https://developer.qiniu.com/fusion/8593/interface-related-certificate
 
-import os
 import json
+from os.path import abspath, dirname
+
+from qiniu import Auth
 import requests
 import yaml
-from qiniu import Auth
 
-with open(os.path.dirname(os.path.abspath(__file__)) + '/config.yaml', 'r') as f:
+with open(dirname(abspath(__file__)) + '/config.yaml', 'r') as f:
     configYamlString = f.read()
 config = yaml.safe_load(configYamlString)
 
+
+# 七牛云相关信息
 AK = config['AK']
 SK = config['SK']
+apiUrlPrefix = 'https://api.qiniu.com'
+apiUrl = f'{apiUrlPrefix}/sslcert'
 
-# 需要把下面的公钥/私钥更新到下面的cdn域名中去
+
+# 需要上传到七牛云的信息，具体可以查看config.yaml.example
 keyFilePath = config['keyFilePath']
 crtFilePath = config['crtFilePath']
 crtName = config['crtName']
 crtCommonName = config['crtCommonName']
 cdnDomains = config['cdnDomains']
 
-# 七牛cdn的api接口url前缀
-apiUrlPrefix = 'https://api.qiniu.com'
 
 with open(keyFilePath, 'r') as f:
     key = f.read()
 with open(crtFilePath, 'r') as f:
     crt = f.read()
 
-# 实例化qiniu Auth对象，方便获得access token
-q = Auth(AK, SK)
 
-# 更新证书
-apiUrl = '{0}/sslcert'.format(apiUrlPrefix)
-
-postData = {
-    'name': crtName,
-    'common_name': crtCommonName,
-    'pri': key,
-    'ca': crt
-}
-
-accessToken = q.token_of_request(apiUrl, postData)
+# 实例化qiniu Auth对象，获得access token
+authObject = Auth(AK, SK)
+postData = {'name': crtName, 'common_name': crtCommonName, 'pri': key, 'ca': crt}
+accessToken = authObject.token_of_request(apiUrl, postData)
 print(accessToken)
 
-headers = {
-    'Authorization': "QBox {0}".format(accessToken),
-    'Content-Type': 'application/json'
-}
 
-resp = json.loads(
-    requests.post(apiUrl, json=postData, headers=headers).text
-)
+# 获取七牛云上所有证书ID，方便后面的更新提交
+headers = {'Authorization': "QBox {0}".format(accessToken), 'Content-Type': 'application/json'}
+resp = json.loads(requests.post(apiUrl, json=postData, headers=headers).text)
 print(resp)
 if resp['code'] != 200:
     raise Exception(resp['error'])
-
-# 获取证书ID
 certId = resp['certID']
 
-# 更新证书到域名
-apiUrl = '{0}/domain/{1}/httpsconf'
+
+# 开始循环更新证书到CDN域名
 for domain in cdnDomains:
-    apiUrlForDomain = apiUrl.format(apiUrlPrefix, domain['name'])
+    apiUrlForDomain = f'{apiUrlPrefix}/domain/{domain["name"]}/httpsconf'
     putData = {
         'certId': certId,
         'forceHttps': domain['forceHttps'],
         'http2Enable': domain['http2Enable']
     }
 
-    accessToken = q.token_of_request(apiUrlForDomain, putData)
+    accessToken = authObject.token_of_request(apiUrlForDomain, putData)
     print(accessToken)
 
     headers = {
@@ -78,7 +67,7 @@ for domain in cdnDomains:
         'Content-Type': 'application/json'
     }
 
-    resp2 = json.loads(
+    print(json.loads(
         requests.put(apiUrlForDomain, json=putData, headers=headers).text
-    )
-    print(resp2)
+    ))
+
